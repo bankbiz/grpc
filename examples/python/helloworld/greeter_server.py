2ab8a125e9df4340742e01066804d419a4d55456
bankbiz <thaworn@graffity.tech>
2023-04-26 21:20:14 +0700

    port = '50052'

--- examples/python/helloworld/greeter_server.py
+++ examples/python/helloworld/greeter_server.py
@@ -28,7 +28,7 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
 
 def serve():
-    port = '50051'
+    port = '50052'
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:' + port)
